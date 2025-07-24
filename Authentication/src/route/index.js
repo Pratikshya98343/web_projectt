21b07a555028023e84c8d478397267a32690cd69
Pratikshya Shrestha <pratikshyashrestha94@gmail.com>
2025-07-24 22:26:29 +0545

    feat: Remove menu and profile management functionality and related routes

--- Authentication/src/route/index.js
+++ Authentication/src/route/index.js
@@ -3,5 +3,3 @@ export * from "./auth/authRoute.js";
 export * from "./category/categoryRoute.js";
 export * from "./product/productRoute.js";
 export * from "./order/orderRoute.js";
-export * from "./menu/menuRoute.js";
-export * from "./profile/Profileroute.js";
